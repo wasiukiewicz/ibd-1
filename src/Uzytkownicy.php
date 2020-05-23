57c99006c57bde4a13a13baa3c0371cd9f53d8a9
Paweł Zawadzki <pawel@zawadzki.name>
2020-05-23 10:46:00 +0200

    zmiana hashowania hasla na bcrypt

--- src/Uzytkownicy.php
+++ src/Uzytkownicy.php
@@ -32,7 +32,7 @@ class Uzytkownicy
             'telefon' => $dane['telefon'],
             'email' => $dane['email'],
             'login' => $dane['login'],
-            'haslo' => md5($dane['haslo']),
+            'haslo' => password_hash($dane['haslo'], PASSWORD_DEFAULT),
             'grupa' => $grupa
         ]);
     }
@@ -47,12 +47,11 @@ class Uzytkownicy
      */
     public function zaloguj($login, $haslo, $grupa)
     {
-        $haslo = md5($haslo);
         $dane = $this->db->pobierzWszystko(
-            "SELECT * FROM uzytkownicy WHERE login = :login AND haslo = '$haslo' AND grupa = '$grupa'", ['login' => $login]
+            "SELECT * FROM uzytkownicy WHERE login = :login AND grupa = '$grupa'", ['login' => $login]
         );
 
-        if ($dane) {
+        if ($dane && password_verify($haslo, $dane[0]['haslo'])) {
             $_SESSION['id_uzytkownika'] = $dane[0]['id'];
             $_SESSION['grupa'] = $dane[0]['grupa'];
             $_SESSION['login'] = $dane[0]['login'];
@@ -142,7 +141,7 @@ class Uzytkownicy
         ];
 
         if (!empty($dane['haslo'])) {
-            $update['haslo'] = md5($dane['haslo']);
+            $update['haslo'] = password_hash($dane['haslo'], PASSWORD_DEFAULT);
         }
 
         return $this->db->aktualizuj('uzytkownicy', $update, $id);
